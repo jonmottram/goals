6d5ded02b8108293357a7a754dbf247c7027a1d6
Jon Mottram <jmottram@real-time-logics-macbook-pro.local>
2012-01-02 18:15:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'f0f4ca8a1fbd3083e4f76c70368bd92d020a9d2f204b788d0ee215add7fc114aa70d0ab68cfb096f0db609550ca4d4fd32cb77306740ecadec893a07bf8b73f1'
